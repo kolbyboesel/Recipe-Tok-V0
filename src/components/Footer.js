c6126a8dbd6b7dbac2c918034543892d8c7873e0
Kolby Boesel <kolbyboesel@Kolbys-MacBook-Pro.local>
2025-03-06 10:19:53 -0600

    Adding instagram and twitter to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaEnvelope, FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <footer className="footer-container">
             <div className="social-links">
+                <a
+                    href="mailto:kolbyzboesel@gmail.com"
+                    aria-label="Email"
+                >
+                    <FaEnvelope size={24} />
+                </a>
                 <a
                     href="https://github.com/kolbyboesel"
                     target="_blank"
@@ -14,18 +20,28 @@ const Footer = () => {
                     <FaGithub size={24} />
                 </a>
                 <a
-                    href="mailto:kolbyzboesel@gmail.com"
-                    aria-label="Email"
-                >
-                    <FaEnvelope size={24} />
-                </a>
-                <a
                     href="https://www.linkedin.com/in/kolby-boesel"
                     target="_blank"
                     rel="noopener noreferrer"
                     aria-label="LinkedIn"
                 >
                     <FaLinkedin size={24} />
+                </a>
+                <a
+                    href="https://www.instagram.com/kolbyboesel/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="Instagram"
+                >
+                    <FaInstagram size={24} />
+                </a>
+                <a
+                    href="https://x.com/kolbyboesel"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="X"
+                >
+                    <FaTwitter />
                 </a>
             </div>
             <p>Site Last Updated: 3/6/2025</p>
